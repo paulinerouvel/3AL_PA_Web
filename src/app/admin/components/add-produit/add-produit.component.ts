a364f8fa777830e74d4079148500a52bc62075be
pauline.rouvel <pauline.rouvel@hotmail.fr>
2019-08-31 13:29:59 +0200

    check the entrepot stock

--- src/app/admin/components/add-produit/add-produit.component.ts
+++ src/app/admin/components/add-produit/add-produit.component.ts
@@ -15,11 +15,11 @@ import { Entrepot } from 'src/app/core/models/entrepot';
 export class AddProduitComponent implements OnInit {
 
   constructor(private entrepotService: EntrepotService, private userService: UserService, private produitService: ProduitService,
-    private storageService : StorageService) { }
+    private storageService: StorageService) { }
 
-  public destinataires : Utilisateur[];
-  public entrepots : Entrepot[];
-  public categoriesProduit : string[];
+  public destinataires: Utilisateur[];
+  public entrepots: Entrepot[];
+  public categoriesProduit: string[];
 
   public productModel = new Produit(0, "", "", "", 0, 0, 0, "", "", 0, "", 0, null, 0, 0);
 
@@ -38,7 +38,7 @@ export class AddProduitComponent implements OnInit {
       if (this.destinataires[i].libelle == "Administrateur" || this.destinataires[i].libelle == "Entreprise" || this.destinataires[i].libelle == "Employe") {
         this.destinataires.splice(i, 1);
         cpt = cpt - 1;
-        i-=1;
+        i -= 1;
       }
 
     }
@@ -52,35 +52,47 @@ export class AddProduitComponent implements OnInit {
   }
 
 
-  async onSubmit(){
+  async onSubmit() {
 
 
 
 
 
-    if(this.productModel.prix <= 0 || this.productModel.quantite <= 0 
-      || new Date(this.productModel.dateMiseEnRayon) < new Date(Date.now()) || new Date(this.productModel.dlc) < new Date(Date.now())){
-      alert('Certains champs ne sont pas valides')
+    if (this.productModel.prix <= 0 || this.productModel.quantite <= 0
+      || new Date(this.productModel.dateMiseEnRayon) < new Date(Date.now()) || new Date(this.productModel.dlc) < new Date(Date.now())) {
+      alert('Certains champs ne sont pas valides');
     }
-    else{
-      let token = this.storageService.getItem('token');
-      this.productModel.photo = "img_product.jpg";
-  
-      if(this.productModel.enRayon == 1){
-        this.productModel.enRayon = true;
-      }
-      else{
-        this.productModel.enRayon = false; 
-      }
-  
-      this.productModel.prixInitial = 0;
-  
-      let res = await this.produitService.addProduct(this.productModel, token).toPromise();
-  
-      console.log(res)
-  
-      alert('Produit ajouté !');
-      location.replace('/gestion-produit');
+    else {
+
+
+      let entrepot: Entrepot = await this.entrepotService.getEntrepotById((this.productModel.entrepotwm_id).toString()).toPromise();
+
+
+      if (entrepot[0].placeLibre + this.productModel.quantite >= entrepot[0].placeTotal) {
+        alert("Il n'y a pas assez de place dans l'entrepôt !");
+      } else {
+
+        let token = this.storageService.getItem('token');
+        this.productModel.photo = "img_product.jpg";
+
+        if (this.productModel.enRayon == 1) {
+          this.productModel.enRayon = true;
+        }
+        else {
+          this.productModel.enRayon = false;
+        }
+
+        this.productModel.prixInitial = 0;
+
+        let res = await this.produitService.addProduct(this.productModel, token).toPromise();
+
+        console.log(res)
+
+        alert('Produit ajouté !');
+        location.replace('/gestion-produit');
+       }
+
+
     }
 
 
--- src/app/admin/components/modif-produit/modif-produit.component.ts
+++ src/app/admin/components/modif-produit/modif-produit.component.ts
@@ -15,73 +15,90 @@ import { Entrepot } from 'src/app/core/models/entrepot';
 })
 export class ModifProduitComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute,private entrepotService: EntrepotService, private userService: UserService, private produitService: ProduitService,
-    private storageService : StorageService) { }
+  constructor(private route: ActivatedRoute, private entrepotService: EntrepotService, private userService: UserService, private produitService: ProduitService,
+    private storageService: StorageService) { }
 
-    public destinataires : Utilisateur[];
-    public entrepots : Entrepot[];
-    public categoriesProduit;
-    photoInit= "";
-  
-    public productModel = new Produit(0, "", "", "", 0, 0, 0, "", "", 0, "", 0, null, 0, 0);
+  public destinataires: Utilisateur[];
+  public entrepots: Entrepot[];
+  public categoriesProduit;
+  photoInit = "";
 
-    async ngOnInit() {
+  public productModel = new Produit(0, "", "", "", 0, 0, 0, "", "", 0, "", 0, null, 0, 0);
 
-
-      let idProduit = this.route.snapshot.queryParamMap.get('id');
-      this.productModel = await this.produitService.getProductById(idProduit).toPromise();
-
-      this.photoInit = this.productModel.photo;
+  async ngOnInit() {
 
 
-      this.productModel.dateMiseEnRayon = this.productModel.dateMiseEnRayon.split('T')[0];
-      this.productModel.dlc = this.productModel.dlc.split('T')[0];
+    let idProduit = this.route.snapshot.queryParamMap.get('id');
+    this.productModel = await this.produitService.getProductById(idProduit).toPromise();
+
+    this.photoInit = this.productModel.photo;
 
 
-      this.entrepots = await this.entrepotService.getAllEntrepot().toPromise();
-  
-      this.destinataires = await this.userService.getAllCategory().toPromise();
-  
-  
-      let cpt = this.destinataires.length;
-      for (let i = 0; i < cpt; i++) {
-  
-        if (this.destinataires[i].libelle == "Administrateur" || this.destinataires[i].libelle == "Entreprise" || this.destinataires[i].libelle == "Employe") {
-          this.destinataires.splice(i, 1);
-          cpt = cpt - 1;
-          i-=1;
+    this.productModel.dateMiseEnRayon = this.productModel.dateMiseEnRayon.split('T')[0];
+    this.productModel.dlc = this.productModel.dlc.split('T')[0];
+
+
+    this.entrepots = await this.entrepotService.getAllEntrepot().toPromise();
+
+    this.destinataires = await this.userService.getAllCategory().toPromise();
+
+
+    let cpt = this.destinataires.length;
+    for (let i = 0; i < cpt; i++) {
+
+      if (this.destinataires[i].libelle == "Administrateur" || this.destinataires[i].libelle == "Entreprise" || this.destinataires[i].libelle == "Employe") {
+        this.destinataires.splice(i, 1);
+        cpt = cpt - 1;
+        i -= 1;
+      }
+
+    }
+
+    this.categoriesProduit = await this.produitService.getAllProductsCategorie().toPromise();
+
+  }
+
+
+  async onSubmit() {
+
+    if (this.productModel.prix <= 0 || this.productModel.quantite <= 0
+      || new Date(this.productModel.dateMiseEnRayon) < new Date(Date.now()) || new Date(this.productModel.dlc) < new Date(Date.now())) {
+      alert('Certains champs ne sont pas valides');
+    }
+    else {
+
+
+      let entrepot: Entrepot = await this.entrepotService.getEntrepotById((this.productModel.entrepotwm_id).toString()).toPromise();
+
+
+
+      if (entrepot[0].placeLibre + this.productModel.quantite >= entrepot[0].placeTotal) {
+        alert("Il n'y a pas assez de place dans l'entrepôt !");
+      } else {
+
+        let token = this.storageService.getItem('token');
+
+        if (this.productModel.photo == null || this.productModel.photo == undefined || this.productModel.photo == "") {
+          this.productModel.photo = this.photoInit;
         }
-  
+
+
+        if (this.productModel.enRayon == 1) {
+          this.productModel.enRayon = true;
+        }
+        else {
+          this.productModel.enRayon = false;
+        }
+
+        this.productModel.prixInitial = 0;
+
+        let res = await this.produitService.updateProduct(this.productModel, token).toPromise();
+
+        console.log(res)
+
+        alert('Produit modifié !');
+        location.replace('/gestion-produit');
       }
-  
-      this.categoriesProduit = await this.produitService.getAllProductsCategorie().toPromise();
-  
     }
-
-
-    async onSubmit(){
-
-      let token = this.storageService.getItem('token');
-
-      if(this.productModel.photo == null || this.productModel.photo == undefined || this.productModel.photo == ""){
-        this.productModel.photo = this.photoInit;
-      }
-      
-  
-      if(this.productModel.enRayon == 1){
-        this.productModel.enRayon = true;
-      }
-      else{
-        this.productModel.enRayon = false; 
-      }
-  
-      this.productModel.prixInitial = 0;
-  
-      let res = await this.produitService.updateProduct(this.productModel, token).toPromise();
-  
-      console.log(res)
-  
-      alert('Produit modifié !');
-      location.replace('/gestion-produit');
-    }
+  }
 }
